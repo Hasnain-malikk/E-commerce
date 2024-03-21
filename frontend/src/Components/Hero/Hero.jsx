7c1c4456c9fb1732d0727912036600a036c4cc57
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-03-21 14:13:34 +0500

    hero added

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_img from '../Assets/hero_image.png'
 
 
 const Hero = () => {
@@ -15,9 +18,13 @@ const Hero = () => {
                 <p>collections</p>
                 <p>for everyone</p>
             </div>
+            <div className="hero-latest-btn">
+              <div>Latest Collection</div>
+              <img src={arrow_icon} alt="" />
+            </div>
         </div>
         <div className="hero-right">
-
+          <img src={hero_img} alt="" />
         </div>
       
     </div>
