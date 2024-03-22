3a431bceda02584930bc69bdb0a03a65ebb93aab
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-03-22 18:19:11 +0500

    shop page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Shop from './Pages/Shop';
 import Product from './Pages/Product'
 import LoginSignUp from './Pages/LoginSignUp';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignUp/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
  
     </div>
