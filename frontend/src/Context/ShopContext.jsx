23700c6a728aaec6dcc75d2ee87248026749503e
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-03-23 17:28:00 +0500

    Context updated

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,19 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_products from '../Components/Assets/all_product.js'
 
 export const ShopContext = createContext(null)
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_products.length; index++) {
+        cart[index] =0;
+    }
+    return cart;
+}
 
 const ShopContextProvider = (props) => {
-    const ContextValue = {all_products};
+    const[cartItems,setCartItems] = useState(getDefaultCart())
+    const ContextValue = {all_products,cartItems};
+    console.log(cartItems)
     return <ShopContext.Provider value={ContextValue}>
         {props.children}
     </ShopContext.Provider>
